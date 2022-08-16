9e331a5c4acb8d3bd427aa987f54a85bbbf5be23
Chasek6 <quranstratton@gmail.com>
2022-08-16 00:23:57 -0400

    added chnages to html file and css

---
+++ Assests/script.js
@@ -0,0 +1,13 @@
+// Variables 
+
+var timerE1 = document.querySelector('.timer')
+ var startButttonE1 =document.querySelector('.inside-btn')
+ var ChoiceButtonE1 = document.querySelector('.choice-btn')
+ var multiBtnE1 = document.querySelector('.multi-btn')
+ var textE1 =document.querySelector('.input')
+ var submitButtonE1 = document.querySelector('.#submit-btn')
+
+
+
+ questions = 
+ ["which "]
